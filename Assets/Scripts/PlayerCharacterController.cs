41a7a9fe3a49d1e7a5b874674eb7e20ed423b596
Teurowel <teurowel@gmail.com>
2020-09-17 07:40:52 +0900

    Lab exercise1

---
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float speed = 10f;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    void Update()
+    {
+        //Set velocity of player
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(horizontalAxis * speed, verticalAxis * speed);
+    }
+
+}
